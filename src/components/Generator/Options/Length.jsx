6e9b0ad0ae525966372863daf6bcfe2e7491eca0
khireddine <khireddinetidjet@gmail.com>
2024-05-18 14:36:54 +0100

    feat(css): update style css

--- src/components/Generator/Options/Length.jsx
+++ src/components/Generator/Options/Length.jsx
@@ -4,12 +4,14 @@ import './Length.css';
 const Length = ({ passwordLength, setPasswordLength }) => {
     return (
         <div className="input-container">
-            <label className="label" htmlFor="length">Characters<span>{passwordLength}</span></label>
+            <label className="label">
+                <span className="length-value">{passwordLength}</span>
+            </label>
             <input
                 type="range"
                 onChange={(e) => setPasswordLength(e.target.value)}
                 min={8}
-                max={24}
+                max={32}
                 step={1}
                 value={passwordLength}
                 className="slider"
--- src/components/PasswordOutput/PasswordOutput.jsx
+++ src/components/PasswordOutput/PasswordOutput.jsx
@@ -5,20 +5,22 @@ import './PasswordOutput.css';
 const PasswordOutput = ({ password, isCopied, setIsCopied }) => {
     return (
         <div className="password-output-container">
-            <input
-                className="password-input"
-                disabled
-                value={password}
-                placeholder="khireddine tidjet"
-            />
-            <CopyToClipboard text={password} onCopy={() => setIsCopied(true)}>
-                <button
-                    className="copy-button"
-                    disabled={!password}
-                >
-                    <Icon icon={isCopied ? "iconamoon:check-bold" : "bxs:copy"} />
-                </button>
-            </CopyToClipboard>
+            <div className="input-button-container">
+                <input
+                    className="password-input"
+                    disabled
+                    value={password}
+                    placeholder="khireddine tidjet"
+                />
+                <CopyToClipboard text={password} onCopy={() => setIsCopied(true)}>
+                    <button
+                        className="copy-button"
+                        disabled={!password}
+                    >
+                        <Icon icon={isCopied ? "iconamoon:check-bold" : "bxs:copy"} />
+                    </button>
+                </CopyToClipboard>
+            </div>
         </div>
     );
 }
